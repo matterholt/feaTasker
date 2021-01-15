eabcdd6d4b7ea3d60f4c6b47f94ee3a309c36531
Matthew Atterholt <29619087+matterholt@users.noreply.github.com>
2021-01-15 16:42:26 -0500

    Initial commit

---
+++ AssignTech.svelte
@@ -0,0 +1,17 @@
+<script>
+export let teamMembers;
+
+export let assignTechInCharge = () => {};
+
+let choosenTic = "";
+</script>
+
+
+    <select name="tic" id="tic" 
+    bind:value={choosenTic}
+    on:change={()=>assignTechInCharge(choosenTic)}>
+      <option  value="none">NONE</option>
+      {#each teamMembers as member}
+        <option value={member}>{member}</option>
+      {/each}
+    </select>
